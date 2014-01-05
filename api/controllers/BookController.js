c2e81419c9a2eead91c78b8c43b00f07bef2847d
KubaZ <kuba.zmudzinski@gmail.com>
2014-01-05 12:23:29 +0100

    fixed whitespace

--- api/controllers/BookController.js
+++ api/controllers/BookController.js
@@ -2,7 +2,7 @@
  * BookController
  *
  * @module      :: Controller
- * @description	:: A set of functions called `actions`.
+ * @description :: A set of functions called `actions`.
  *
  *                 Actions contain code telling Sails how to respond to a certain type of request.
  *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
@@ -24,43 +24,43 @@ module.exports = {
   _config: {},
 
   create: function (req, res) {
-  	https.get('https://www.googleapis.com/books/v1/volumes?q=ISBN+' +
-  		req.param('isbn') + '&key=' + sails.config.googleBooksApiKey, function(response) {
-	  var data = '',
-	  	  categories = [];
+    https.get('https://www.googleapis.com/books/v1/volumes?q=ISBN+' +
+      req.param('isbn') + '&key=' + sails.config.googleBooksApiKey, function(response) {
+      var data = '',
+          categories = [];
 
-	  response.on('data', function(d) {
-	  	data += d;
-	  });
+      response.on('data', function(d) {
+        data += d;
+      });
 
-	  response.on('end', function() {
-	  	var body = JSON.parse(data);
+      response.on('end', function() {
+        var body = JSON.parse(data);
 
-	  	console.log(body);
-	  	if (body.items) {
-	  		if (body.items[0].volumeInfo.categories) {
-	            categories = body.items[0].volumeInfo.categories;
-	        }
+        console.log(body);
+        if (body.items) {
+          if (body.items[0].volumeInfo.categories) {
+                categories = body.items[0].volumeInfo.categories;
+            }
 
-		  	Book.create({
-		  		isbn: req.param('isbn'),
-		        title: body.items[0].volumeInfo.title,
-		        authors: body.items[0].volumeInfo.authors,
-		        categories: categories
-		  	}).done(function (error, book) {
-		  		if (error) {
-		  			res.send(error, 500);
-		  		} else {
-		  			res.json(book);
-		  		}
-		  	});
-		} else {
-			res.send('Not Found', 404);
-		}
-	  });
-	}).on('error', function(e) {
-	  console.error(e);
-	  res.json(e);
-	});
+          Book.create({
+            isbn: req.param('isbn'),
+              title: body.items[0].volumeInfo.title,
+              authors: body.items[0].volumeInfo.authors,
+              categories: categories
+          }).done(function (error, book) {
+            if (error) {
+              res.send(error, 500);
+            } else {
+              res.json(book);
+            }
+          });
+        } else {
+          res.send('Not Found', 404);
+        }
+      });
+    }).on('error', function(e) {
+      console.error(e);
+      res.json(e);
+    });
   }
 };
